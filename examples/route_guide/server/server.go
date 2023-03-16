9a00b184b1cb64120cba3d66b4012e6321bf12a5
Michael Kalantar <kalantar@us.ibm.com>
2023-03-16 14:45:19 -0400

    allow requests to routeguide.default
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- examples/route_guide/server/server.go
+++ examples/route_guide/server/server.go
@@ -219,7 +219,7 @@ func newServer() *routeGuideServer {
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("routeguide.default:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
